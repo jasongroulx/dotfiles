00494d282c1f5ea6638dbcb428050fc67002c62b
Jason Groulx <groulx.jason@gmail.com>
2018-07-11 15:51:36 -0400

    Added gitconfig, gitignore_global and ctags symlinks

--- symlinks.sh
+++ symlinks.sh
@@ -12,6 +12,7 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 sudo rm -rf ~/Library/Application\ Support/Spectacle/Shortcuts.json > /dev/null 2>&1
 sudo rm -rf ~/.vimrc > /dev/null 2>&1
 sudo rm -rf ~/.zshrc > /dev/null 2>&1
+sudo rm -rf ~/.gitconfig > /dev/null 2>&1
 sudo rm -rf ~/.config/karabiner/karabiner.json > /dev/null 2>&1
 
 #==============
@@ -20,6 +21,8 @@ sudo rm -rf ~/.config/karabiner/karabiner.json > /dev/null 2>&1
 ln -sf ~/.dotfiles/Spectacle/Shortcuts.json ~/Library/Application\ Support/Spectacle
 ln -sf ~/.dotfiles/.vimrc ~/.vimrc
 ln -sf ~/.dotfiles/.zshrc ~/.zshrc
+ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/.dotfiles/.nvmrc ~/.nvmrc
 ln -sf ~/.dotfiles/.my.cnf ~/.my.cnf
 ln -sf ~/.dotfiles/karabiner/karabiner.json ~/.config/karabiner
+ln -sf ~/.dotfiles/.ctags ~/.ctags
