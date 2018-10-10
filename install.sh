c69f13e2dc4a06dfc82b63a2a1b405eb786fc74a
Jason Groulx <groulx.jason@gmail.com>
2018-10-10 16:16:17 -0400

    Added virtualenv to install script & pip upgrade

--- install.sh
+++ install.sh
@@ -41,7 +41,8 @@ valet park ~/code
 npm install --global yarn sass
 
 # Install global python packages
-pip install yapf
+pip install --upgrade pip
+pip install yapf virtualenv
 
 # Install nvm
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
