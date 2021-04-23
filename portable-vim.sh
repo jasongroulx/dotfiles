e2a7e65795b790f81f80f9a6dc2626a2c6276aad
Jason Groulx <groulx.jason@gmail.com>
2021-04-22 23:46:36 -0400

    Another one

--- portable-vim.sh
+++ portable-vim.sh
@@ -3,6 +3,6 @@ export MYVIMRC=~/.jasongroulx/vim/init.vim
 export VIMFILES=~/.jasongroulx/vim/portable
 export VIMDOTFILES=~/.jasongroulx/vim
 
-[ "$(grep '^alias jvim=' ~/.zshrc)" ] || echo "\n# Load nvim with .jasongroulx config \nalias jvim='nvim -u ~/.jasongroulx/vim/init.vim'" >> ~/.zshrc
+[ "$(grep '^alias jvim=' ~/.zshrc)" ] || echo "\n# Load nvim with .jasongroulx config \nalias jvim='~/.jasongroulx/portable-vim.sh'" >> ~/.zshrc
 
-# nvim -u ~/.jasongroulx/vim/init.vim 
+nvim -u ~/.jasongroulx/vim/init.vim 
